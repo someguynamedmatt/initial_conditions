e1712376b2c00aadee487e19516154075d2b4f67
Matt <young.qubit@gmail.com>
2018-02-22 22:43:28 -0800

    Export consts to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
 mod consts;
 
+pub mod _const {
+    pub use consts::{
+        LIGHT_SPEED,
+        G,
+        LITTLE_G,
+        ELECTRON_MASS,
+        PROTON_MASS,
+        ELECTRON_CHARGE,
+        PLANCKS_CONST,
+        EARTH_MASS,
+        EARTH_RADIUS,
+        EARTH_RADIUS_ORBIT,
+        EARTH_PERIOD_ROT,
+        EARTH_PERIOD_REV,
+        MOON_MASS,
+        MOON_RAD,
+        MOON_ORBIT_RAD,
+        SUN_MASS,
+        ANGSTROM,
+        AMU_KG,
+        AMU_MEV
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::consts as _const;
